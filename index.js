f8b688c88f15b5094b295779043e5f7859e36d90
student_123 <student_123@admins-Air-5.home>
2019-01-10 20:48:16 -0500

    fixed env issue and created message function

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('message', function(message){
+  if (message.content == 'Hello') {
+    message.reply('Hello there')
+  }
+});
