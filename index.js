c2bc49945d7085a2b88ff7d8b47d8d5bdab01340
student_123 <student_123@ool-ad020780.dyn.optonline.net>
2019-01-11 13:56:30 -0500

    error handling bot

--- index.js
+++ index.js
@@ -2,10 +2,33 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
-bot.login(TOKEN);
+let arena;
+let pw;
 
-bot.on('message', function(message){
-  if (message.content == 'Hello') {
-    message.reply('Hello there')
+bot.on('message', function(message, arendaId, password){
+  userMessage = message.content.split(' ');
+  callSmashBot = `${userMessage[0]} ${userMessage[1]}`;
+  keywords = `${userMessage[0]} ${userMessage[1]} ${userMessage[2]}`;
+
+  if (callSmashBot.toLowerCase() == 'smash bot') {
+    if (userMessage.length > 2) {
+      // Detect what command was said after calling smash bot
+      if (userMessage[2].toLowerCase() == 'arena') {
+        // if (userMessage.length == 5) {
+        //   arena = userMessage[3];
+        //   pw = userMessage[4];
+        //   message.channel.send(`Arena created!\nArenaID: ${arena}\nPassword: ${pw}`);
+        // } else if (userMessage.length > 5){
+        //   message.channel.send('Error: Detected too many values. Please make sure you type the arenaID followed by the Password when creating an arena');
+        // }
+        message.channel.send('what\'s the ID and Password?');
+      }
+    } else {
+      // if message is only 'smash bot' respond with instructions on how to use smash bot
+      message.channel.send('Smash bot instructions');
+    }
   }
+
 });
+
+bot.login(TOKEN);
