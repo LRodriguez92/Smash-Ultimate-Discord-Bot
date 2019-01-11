eba9662bc4c7a8bef31534ad4ac7ef58222df094
student_123 <student_123@ool-ad020780.dyn.optonline.net>
2019-01-11 16:22:38 -0500

    created arenaCommand function

--- index.js
+++ index.js
@@ -2,30 +2,36 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
-let arena;
-let pw;
-
 bot.on('message', function(message, arendaId, password){
-  userMessage = message.content.split(' ');
-  callSmashBot = userMessage[0];
-  command = userMessage[1];
+  let userMessage = message.content.split(' ');
+  let callSmashBot = userMessage[0];
+  let command = userMessage[1];
 
   if (callSmashBot.toLowerCase() == '!smash') {
-    if (userMessage.length >= 2) {
-      // Detect what command was said after calling smash bot
-      if (command.toLowerCase() == 'arena') {
-        // if (userMessage.length == 5) {
-        //   arena = userMessage[3];
-        //   pw = userMessage[4];
-        //   message.channel.send(`Arena created!\nArenaID: ${arena}\nPassword: ${pw}`);
-        // } else if (userMessage.length > 5){
-        //   message.channel.send('Error: Detected too many values. Please make sure you type the arenaID followed by the Password when creating an arena');
-        // }
-        message.channel.send('To use the \'arena\' command type: !smash arena [ID] [Password]');
-      }
+    if (userMessage.length >= 2) { // Detects command input after calling smash bot
+      arenaCommand();
     } else {
       // if message is only '!smash bot' respond with instructions on how to use smash bot
       message.channel.send('Smash bot instructions');
+    }
+  }
+
+  function arenaCommand() {
+    if (command.toLowerCase() == 'arena') {
+      let arena, pw;
+      if (userMessage.length == 4) {
+        arena = userMessage[2];
+        pw = userMessage[3];
+        message.channel.send(`Arena created!\nArenaID: ${arena}\nPassword: ${pw}`);
+      } else if (userMessage.length > 4) {
+        message.channel.send('Too many values, human! I only need the arena [ID] followed by the [Password] when creating an arena');
+      } else if (userMessage.length == 3) {
+        message.channel.send('No password detected! If you wish to not have a password, enter \'open\' as your [Password]');
+      } else {
+        message.channel.send('To use the <arena> command, type: !smash arena [ID] [Password]');
+      }
+    } else {
+      message.channel.send(`You fool! The <${command}> command does not exist!`)
     }
   }
 
