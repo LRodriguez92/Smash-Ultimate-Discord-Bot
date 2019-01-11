439e22e74a992907391b1f2fd985bad01ae638a8
student_123 <student_123@ool-ad020780.dyn.optonline.net>
2019-01-11 15:46:31 -0500

    added callSmashBot and command variables and arena if statement

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ let pw;
 
 bot.on('message', function(message, arendaId, password){
   userMessage = message.content.split(' ');
-  callSmashBot = `${userMessage[0]} ${userMessage[1]}`;
-  keywords = `${userMessage[0]} ${userMessage[1]} ${userMessage[2]}`;
+  callSmashBot = userMessage[0];
+  command = userMessage[1];
 
-  if (callSmashBot.toLowerCase() == 'smash bot') {
-    if (userMessage.length > 2) {
+  if (callSmashBot.toLowerCase() == '!smash') {
+    if (userMessage.length >= 2) {
       // Detect what command was said after calling smash bot
-      if (userMessage[2].toLowerCase() == 'arena') {
+      if (command.toLowerCase() == 'arena') {
         // if (userMessage.length == 5) {
         //   arena = userMessage[3];
         //   pw = userMessage[4];
@@ -21,10 +21,10 @@ bot.on('message', function(message, arendaId, password){
         // } else if (userMessage.length > 5){
         //   message.channel.send('Error: Detected too many values. Please make sure you type the arenaID followed by the Password when creating an arena');
         // }
-        message.channel.send('what\'s the ID and Password?');
+        message.channel.send('To use the \'arena\' command type: !smash arena [ID] [Password]');
       }
     } else {
-      // if message is only 'smash bot' respond with instructions on how to use smash bot
+      // if message is only '!smash bot' respond with instructions on how to use smash bot
       message.channel.send('Smash bot instructions');
     }
   }
