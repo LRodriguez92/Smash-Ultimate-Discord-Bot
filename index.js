ebc581220c437794436bf5e2bc00b1c807b1d18f
student_123 <student_123@ool-ad020780.dyn.optonline.net>
2019-01-14 12:21:04 -0500

    can close the current user's arena

--- index.js
+++ index.js
@@ -28,46 +28,67 @@ bot.on('message', function(message){
         arena = userMessage[2];
         password = userMessage[3];
         if (arena.length > 5) {
-          message.channel.send(`Invalid Arend [ID]! Too many characters`);
+          message.channel.send(`Invalid Arend [ID]! Too many characters.`);
         } else if (arena.length < 5) {
-          message.channel.send(`Invalid Arena [ID]! Not enough characters`);
+          message.channel.send(`Invalid Arena [ID]! Not enough characters.`);
         } else if (specialChar.test(arena)){
-          message.channel.send(`Invalid Arena [ID]! You know damn well you can't use special characters in an [ID]`)
+          message.channel.send(`Invalid Arena [ID]! You know damn well you can't use special characters in an [ID].`)
         } else if (password.length > 8){
           message.channel.send(`Invalid [Password]! Passwords can't exceed 8 characters!`);
         } else if (specialChar.test(password) || isNaN(password)){
           message.channel.send(`Invalid [Password]! Passwords can only be numbers genius!`)
-        } else { // Save arena info
+        } else {
+          // Save arena info
           if (arenas.length == 0) {
             arenas.push({user: messageUser, arena: arena, password: password});
-            message.channel.send(`\n${messageUser}'s created an arena:\n\n[ArenaID]: ${arena}\n[Password]: ${password}`);
+            message.channel.send(`\n${messageUser} created an arena:\n\n[ArenaID]: ${arena}\n[Password]: ${password}`);
           } else {
             arenas.map(currentArena => {
               if (messageUser == currentArena.user) {
                 currentArena.arena = arena;
                 currentArena.password = password;
-                message.channel.send(`\n${messageUser}'s updated their arena:\n\n[ArenaID]: ${arena}\n[Password]: ${password}`);
+                message.channel.send(`\n${messageUser} updated their arena:\n\n[ArenaID]: ${arena}\n[Password]: ${password}`);
               } else {
                 arenas.push({user: messageUser, arena: arena, password: password});
-                message.channel.send(`\n${messageUser}'s created an arena:\n\n[ArenaID]: ${arena}\n[Password]: ${password}`);
+                message.channel.send(`\n${messageUser} created an arena:\n\n[ArenaID]: ${arena}\n[Password]: ${password}`);
               }
             });
           }
         }
       } else if (userMessage.length > 4) {
-        message.channel.send('Too many values, human! I only need the arena [ID] followed by the [Password] when creating an arena');
-      } else if (userMessage.length == 3 && userMessage[2] != 'show' && userMessage[2].length == 5) {
-        message.channel.send('No password detected! If you wish to not have a password, enter \'open\' as your [Password]');
-      } else if (userMessage.length == 3 && userMessage[2] == 'show') { // Show current arenas
+        // If there are too many values after arena command
+        message.channel.send('Too many values, human! I only need the arena [ID] followed by the [Password] when creating an arena.');
+      } else if (userMessage.length == 3 && userMessage[2] != 'show' && userMessage[2] != 'close' && userMessage[2].length == 5) {
+        // If the user did not input a password
+        message.channel.send('No password detected! If you wish to not have a password, enter \'open\' as your [Password].');
+      } else if (userMessage.length == 3 && userMessage[2] == 'show') {
+        // Show current arenas
         if (arenas.length == 0) {
-          message.channel.send(`No arenas open at the moment. Feel free to create one`)
+          message.channel.send(`No arenas open at the moment. Feel free to create one.`)
         } else {
+          // Shows all open arenas
           message.channel.send('Current arena(s) open:');
           arenas.map(currentArena => {
             message.channel.send(`\n${currentArena.user}'s arena:\n\n[ArenaID]: ${currentArena.arena}\n[Password]: ${currentArena.password}\n\n-----------------------------------------`);
           });
         }
-      } else {
+      } else if (userMessage.length == 3 && userMessage[2] == 'close') {
+        // Close arena the user has created
+        if (arenas.length == 0) {
+          message.channel.send(`There are no open arenas.`)
+        } else {
+          // Finds the user's arena and closes it
+          message.channel.send(`${messageUser}'s arena has been closed.`);
+          let i = 0;
+          arenas.map(currentArena => {
+            if (currentArena.user == messageUser) {
+              arenas.splice(i, 1);
+            } else {
+              i++;
+            }
+          });
+        }
+      }  else {
         message.channel.send('To use the <arena> command, type: !smash arena [ID] [Password]');
       }
     } else {
